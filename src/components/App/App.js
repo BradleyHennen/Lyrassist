79efa1495d5b6b6b8819f8a4a8be1e8379b939a2
Bradley Hennen <bradhennen5@gmail.com>
2019-04-22 13:03:21 -0500

    added welcome page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -16,6 +16,7 @@ import ProtectedRoute from '../ProtectedRoute/ProtectedRoute'
 import AboutPage from '../AboutPage/AboutPage';
 import UserPage from '../UserPage/UserPage';
 import InfoPage from '../InfoPage/InfoPage';
+import Welcome from '../Welcome/Welcome';
 
 import './App.css';
 
@@ -27,41 +28,43 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div>
-          <Nav />
-          <Switch>
-            {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
-            <Redirect exact from="/" to="/home" />
-            {/* Visiting localhost:3000/about will show the about page.
-            This is a route anyone can see, no login necessary */}
-            <Route
-              exact
-              path="/about"
-              component={AboutPage}
-            />
-            {/* For protected routes, the view could show one of several things on the same route.
-            Visiting localhost:3000/home will show the UserPage if the user is logged in.
-            If the user is not logged in, the ProtectedRoute will show the 'Login' or 'Register' page.
-            Even though it seems like they are different pages, the user is always on localhost:3000/home */}
-            <ProtectedRoute
-              exact
-              path="/home"
-              component={UserPage}
-            />
-            {/* This works the same as the other protected route, except that if the user is logged in,
-            they will see the info page instead. */}
-            <ProtectedRoute
-              exact
-              path="/info"
-              component={InfoPage}
-            />
-            {/* If none of the other routes matched, we will show a 404. */}
-            <Route render={() => <h1>404</h1>} />
-          </Switch>
-          <Footer />
-        </div>
+        <Route exact path="/" component={Welcome}/>
       </Router>
   )}
 }
 
 export default connect()(App);
+
+// <div>
+// <Nav />
+// <Switch>
+//   {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
+//   <Redirect exact from="/" to="/home" />
+//   {/* Visiting localhost:3000/about will show the about page.
+//   This is a route anyone can see, no login necessary */}
+//   <Route
+//     exact
+//     path="/about"
+//     component={AboutPage}
+//   />
+//   {/* For protected routes, the view could show one of several things on the same route.
+//   Visiting localhost:3000/home will show the UserPage if the user is logged in.
+//   If the user is not logged in, the ProtectedRoute will show the 'Login' or 'Register' page.
+//   Even though it seems like they are different pages, the user is always on localhost:3000/home */}
+//   <ProtectedRoute
+//     exact
+//     path="/home"
+//     component={UserPage}
+//   />
+//   {/* This works the same as the other protected route, except that if the user is logged in,
+//   they will see the info page instead. */}
+//   <ProtectedRoute
+//     exact
+//     path="/info"
+//     component={InfoPage}
+//   />
+//   {/* If none of the other routes matched, we will show a 404. */}
+//   <Route render={() => <h1>404</h1>} />
+// </Switch>
+// <Footer />
+// </div>
--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,24 +1,64 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
-class LoginPage extends Component {
-
-  render() {
-    return (
-      <div>
-        <header>
-            
-        </header>
-      </div>
-    );
-  }
-}
-
-// Instead of taking everything from state, we just want the error messages.
-// if you wanted you could write this code like this:
-// const mapStateToProps = ({errors}) => ({ errors });
-const mapStateToProps = state => ({
-  errors: state.errors,
+const styles = theme => ({
+    root: {
+        ...theme.mixins.gutters(),
+        paddingTop: theme.spacing.unit * 2,
+        paddingBottom: theme.spacing.unit * 2,
+        marginRight: '4%',
+        marginLeft: '4%',
+    },
+    header: {
+        textAlign: "center",
+    },
+    description: {
+        textAlign: "center",
+        width: "50%",
+        height: "25%",
+        marginLeft: "auto",
+        marginRight: "auto",
+        display: "block",
+    },
+    images: {
+        width: "25%",
+        height: "25%",
+        marginLeft: "auto",
+        marginRight: "auto",
+        display: "block",
+    }
 });
 
-export default connect(mapStateToProps)(LoginPage);
+class Welcome extends Component {
+
+
+    render() {
+        const { classes } = this.props;
+
+        return (
+            <div>
+                <Paper className={classes.root}>
+                    <Typography variant="h2" className={classes.header}>LyrAssist</Typography>
+                </Paper>
+                <Paper className={classes.root}>
+                    <img className={classes.images} src="/images/LyrAssist_logo.svg" alt="lyrAssist Logo"/>
+                    <Typography variant="body1" className={classes.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tristique lectus nulla, vitae accumsan neque luctus quis. Ut rhoncus, libero id tristique ornare, odio sapien tincidunt enim, ac mattis elit ligula eget nulla. Suspendisse tempus arcu magna, non aliquet ex molestie sed. Aliquam vel dolor eget lorem sollicitudin ultricies vel et nisl. Proin ac enim tincidunt, pellentesque lectus nec, laoreet nisl. Donec eros leo, fringilla et odio sed, ultrices placerat eros. Cras tincidunt risus lectus, quis placerat risus sagittis sit amet. Suspendisse finibus diam eget augue gravida placerat. Mgula vel, vestibulum ultrices felis. Nulla semper maximus consequat. Nunc in malesuada risus. Nullam viverra ornare nulla, non cursus felis convallis sit amet. Nullam consequat ligula ante.</Typography>
+                </Paper>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => ({
+    errors: state.errors,
+});
+
+Welcome.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default connect(mapStateToProps)(withStyles(styles)(Welcome));
