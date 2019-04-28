d590ef0a3546430d39dddab22c1d9d2c9df81f64
Bradley Hennen <bradhennen5@gmail.com>
2019-04-28 10:10:59 -0500

    added table soriting

--- src/components/AssistantResults/AssistantResults.js
+++ src/components/AssistantResults/AssistantResults.js
@@ -22,6 +22,87 @@ import FilterListIcon from '@material-ui/icons/FilterList';
 import { lighten } from '@material-ui/core/styles/colorManipulator';
 import TablePaginationActions from './TablePagination';
 
+function desc(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function stableSort(array, cmp) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = cmp(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map(el => el[0]);
+}
+
+function getSorting(order, orderBy) {
+  return order === 'desc' ? (a, b) => desc(a, b, orderBy) : (a, b) => -desc(a, b, orderBy);
+}
+
+const rows = [
+  { id: 'word', numeric: false, disablePadding: true, label: 'Word/Phrase' },
+  { id: 'score', numeric: true, disablePadding: false, label: 'Ranking' },
+  { id: 'numSyllables', numeric: true, disablePadding: false, label: 'Syllables' },
+];
+
+class EnhancedTableHead extends React.Component {
+  createSortHandler = property => event => {
+    this.props.onRequestSort(event, property);
+  };
+
+  render() {
+    const { order, orderBy } = this.props;
+
+    return (
+      <TableHead>
+        <TableRow>
+          {rows.map(
+            row => (
+              <TableCell
+                key={row.id}
+                align={row.numeric ? 'right' : 'left'}
+                sortDirection={orderBy === row.id ? order : false}
+              >
+                <Tooltip
+                  title="Sort"
+                  placement={row.numeric ? 'bottom-end' : 'bottom-start'}
+                  enterDelay={300}
+                >
+                  <TableSortLabel
+                    active={orderBy === row.id}
+                    direction={order}
+                    onClick={this.createSortHandler(row.id)}
+                  >
+                    {row.label}
+                  </TableSortLabel>
+                </Tooltip>
+              </TableCell>
+            ),
+            this,
+          )}
+        </TableRow>
+      </TableHead>
+    );
+  }
+}
+
+EnhancedTableHead.propTypes = {
+  numSelected: PropTypes.number.isRequired,
+  onRequestSort: PropTypes.func.isRequired,
+  onSelectAllClick: PropTypes.func.isRequired,
+  order: PropTypes.string.isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired,
+};
+
+
 
 TablePaginationActions.propTypes = {
   classes: PropTypes.object.isRequired,
@@ -36,13 +117,16 @@ const styles = theme => ({
   root: {
     width: '100%',
     overflowX: 'auto',
+    marginTop: theme.spacing.unit,
   },
 });
 
 class AssistantResults extends Component {
 
   state = {
-    rows: [],
+    order: 'asc',
+    orderBy: 'ranking',
+    data: [],
     page: 0,
     rowsPerPage: 8,
   }
@@ -51,13 +135,45 @@ class AssistantResults extends Component {
     this.props.dispatch({ type: 'GET_DATAMUSE' })
   }
 
-  componentDidUpdate(prevProps) {   
+  componentDidUpdate(prevProps) {
     if (this.props.reduxState.datamuseData !== prevProps.reduxState.datamuseData) {
-        this.setState({
-            rows: this.props.reduxState.datamuseData,
-        })
+      this.setState({
+        data: this.props.reduxState.datamuseData,
+      })
     }
-}
+  }
+
+  handleRequestSort = (event, property) => {
+    const orderBy = property;
+    let order = 'desc';
+
+    if (this.state.orderBy === property && this.state.order === 'desc') {
+      order = 'asc';
+    }
+
+    this.setState({ order, orderBy });
+  };
+
+  handleClick = (event, id) => {
+    const { selected } = this.state;
+    const selectedIndex = selected.indexOf(id);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, id);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1),
+      );
+    }
+
+    this.setState({ selected: newSelected });
+  };
 
   handleChangePage = (event, page) => {
     this.setState({ page });
@@ -70,7 +186,7 @@ class AssistantResults extends Component {
 
   render() {
     const { classes } = this.props;
-    const { rows, rowsPerPage, page } = this.state;
+    const {  data, order, orderBy, rowsPerPage, page } = this.state;
     const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
 
 
@@ -79,35 +195,38 @@ class AssistantResults extends Component {
         <Typography variant="h2" align="center">Results</Typography>
         <Paper className={classes.root}>
           <Table className={classes.table}>
-            <TableHead>
-              <TableRow>
-                <TableCell>Word/Phrase</TableCell>
-                <TableCell align="right">Ranking</TableCell>
-                <TableCell align="right">Syllables</TableCell>
-              </TableRow>
-            </TableHead>
+            <EnhancedTableHead
+              order={order}
+              orderBy={orderBy}
+              onRequestSort={this.handleRequestSort}
+              rowCount={data.length}/>
             <TableBody>
-              {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row, i) => (
-                <TableRow key={i}>
-                  <TableCell component="th" scope="row">
-                    {row.word}
-                  </TableCell>
-                  <TableCell align="right">{row.score}</TableCell>
-                  <TableCell align="right">{row.numSyllables}</TableCell>
-                </TableRow>
-              ))}
-              {emptyRows > 0 && (
-                <TableRow style={{ height: 48 * emptyRows }}>
-                  <TableCell colSpan={6} />
-                </TableRow>
-              )}
+              {stableSort(data, getSorting(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((n, index) => {
+                  return (
+                    <TableRow
+                      hover
+                      onClick={event => this.handleClick(event, index)}
+                      role="checkbox"
+                      tabIndex={-1}
+                      key={index}
+                    >
+                      <TableCell component="th" scope="row" >
+                        {n.word}
+                      </TableCell>
+                      <TableCell align="right">{n.score}</TableCell>
+                      <TableCell align="right">{n.numSyllables}</TableCell>
+                    </TableRow>
+                  );
+                })}
             </TableBody>
             <TableFooter>
               <TableRow>
                 <TablePagination
-                  rowsPerPageOptions={[8, 10, 25]}
+                  rowsPerPageOptions={[10, 15, 20, 25, 30]}
                   colSpan={3}
-                  count={rows.length}
+                  count={data.length}
                   rowsPerPage={rowsPerPage}
                   page={page}
                   SelectProps={{
