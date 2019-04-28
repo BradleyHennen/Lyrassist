163357c7b085f4d13c04e15390ef4ee40d280c65
Bradley Hennen <bradhennen5@gmail.com>
2019-04-27 22:33:53 -0500

    results table now has pages

--- src/components/AssistantResults/AssistantResults.js
+++ src/components/AssistantResults/AssistantResults.js
@@ -8,65 +8,212 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-// import Typography from '@material-ui/core/Typography';
-// import Button from '@material-ui/core/Button';
-// import Grid from '@material-ui/core/Grid';
+import TableFooter from '@material-ui/core/TableFooter';
+import TablePagination from '@material-ui/core/TablePagination';
+import IconButton from '@material-ui/core/IconButton';
+import FirstPageIcon from '@material-ui/icons/FirstPage';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import LastPageIcon from '@material-ui/icons/LastPage';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Toolbar from '@material-ui/core/Toolbar';
+import Tooltip from '@material-ui/core/Tooltip';
+import FilterListIcon from '@material-ui/icons/FilterList';
+import { lighten } from '@material-ui/core/styles/colorManipulator';
+
+
+const actionsStyles = theme => ({
+  root: {
+    flexShrink: 0,
+    color: theme.palette.text.secondary,
+    marginLeft: theme.spacing.unit * 2.5,
+  },
+});
+
+class TablePaginationActions extends Component {
+  handleFirstPageButtonClick = event => {
+    this.props.onChangePage(event, 0);
+  };
+
+  handleBackButtonClick = event => {
+    this.props.onChangePage(event, this.props.page - 1);
+  };
+
+  handleNextButtonClick = event => {
+    this.props.onChangePage(event, this.props.page + 1);
+  };
+
+  handleLastPageButtonClick = event => {
+    this.props.onChangePage(
+      event,
+      Math.max(0, Math.ceil(this.props.count / this.props.rowsPerPage) - 1),
+    );
+  };
+
+  render() {
+    const { classes, count, page, rowsPerPage, theme } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <IconButton
+          onClick={this.handleFirstPageButtonClick}
+          disabled={page === 0}
+          aria-label="First Page"
+        >
+          {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+        </IconButton>
+        <IconButton
+          onClick={this.handleBackButtonClick}
+          disabled={page === 0}
+          aria-label="Previous Page"
+        >
+          {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+        </IconButton>
+        <IconButton
+          onClick={this.handleNextButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="Next Page"
+        >
+          {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+        </IconButton>
+        <IconButton
+          onClick={this.handleLastPageButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="Last Page"
+        >
+          {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+        </IconButton>
+      </div>
+    );
+  }
+}
+
+TablePaginationActions.propTypes = {
+  classes: PropTypes.object.isRequired,
+  count: PropTypes.number.isRequired,
+  onChangePage: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+  theme: PropTypes.object.isRequired,
+};
+
+const TablePaginationActionsWrapped = withStyles(actionsStyles, { withTheme: true })(
+  TablePaginationActions,
+);
+
+let counter = 0;
+function createData(name, calories, fat) {
+  counter += 1;
+  return { id: counter, name, calories, fat };
+}
 
 const styles = theme => ({
-    root: {
-      width: '100%',
-      // marginTop: theme.spacing.unit * 3,
-      overflowX: 'auto',
-    },
-    table: {
-      // minWidth: 700,
-    },
-  });
+  root: {
+    width: '100%',
+    // marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+    // minWidth: 700,
+  },
+});
+
+
+
 
 class AssistantResults extends Component {
 
-    componentDidMount = () => {
-        this.props.dispatch({type: 'GET_DATAMUSE'})
-    }
-    
-    render() {
-        const { classes } = this.props;
-        return (
-            <div >
-            <Typography variant="h2" align="center">Results</Typography>
-            <Paper className={classes.root}>
-            <Table className={classes.table}>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Word/Phrase</TableCell>
-                  <TableCell align="right">Ranking</TableCell>
-                  <TableCell align="right">Syllables</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {this.props.reduxState.datamuseData.map((datamuse, i) => (
-                  <TableRow key={i}>
-                    <TableCell component="th" scope="row">
-                      {datamuse.word}
-                    </TableCell>
-                    <TableCell align="right">{datamuse.score}</TableCell>
-                    <TableCell align="right">{datamuse.numSyllables}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </Paper>
-          </div>
-        );
+  state = {
+    rows: [],
+    page: 0,
+    rowsPerPage: 8,
+  }
+
+  componentDidMount = () => {
+    this.props.dispatch({ type: 'GET_DATAMUSE' })
+  }
+
+  componentDidUpdate(prevProps) {   
+    if (this.props.reduxState.datamuseData !== prevProps.reduxState.datamuseData) {
+        this.setState({
+            rows: this.props.reduxState.datamuseData,
+        })
     }
 }
 
+  handleChangePage = (event, page) => {
+    this.setState({ page });
+  };
+
+  handleChangeRowsPerPage = event => {
+    this.setState({ page: 0, rowsPerPage: event.target.value });
+  };
+
+
+  render() {
+    const { classes } = this.props;
+    const { rows, rowsPerPage, page } = this.state;
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+
+
+    return (
+      <div >
+        <Typography variant="h2" align="center">Results</Typography>
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>Word/Phrase</TableCell>
+                <TableCell align="right">Ranking</TableCell>
+                <TableCell align="right">Syllables</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => (
+                <TableRow key={row.id}>
+                  <TableCell component="th" scope="row">
+                    {row.word}
+                  </TableCell>
+                  <TableCell align="right">{row.score}</TableCell>
+                  <TableCell align="right">{row.numSyllables}</TableCell>
+                </TableRow>
+              ))}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: 48 * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TablePagination
+                  rowsPerPageOptions={[8, 10, 25]}
+                  colSpan={3}
+                  count={rows.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  SelectProps={{
+                    native: true,
+                  }}
+                  onChangePage={this.handleChangePage}
+                  onChangeRowsPerPage={this.handleChangeRowsPerPage}
+                  ActionsComponent={TablePaginationActionsWrapped}
+                />
+              </TableRow>
+            </TableFooter>
+          </Table>
+        </Paper>
+      </div>
+    );
+  }
+}
+
 const mapStateToProps = reduxState => ({
-    reduxState
+  reduxState
 });
 
 AssistantResults.propTypes = {
-    classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
 };
 
 export default connect(mapStateToProps)(withStyles(styles)(AssistantResults));
